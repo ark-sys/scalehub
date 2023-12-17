2a113e4c525a0de788835f90ab18af60077fc04e
Khaled Arsalane <contact@khaled.ovh>
2023-12-17 10:52:44 +0100

    Testing minio multi-tenants colocated with flink

--- script/src/G5k.py
+++ script/src/G5k.py
@@ -53,12 +53,6 @@ class G5k(Platform):
                 nodes=self.workers,
                 primary_network=network,
             )
-            .add_machine(
-                roles=["storage"],
-                cluster=self.cluster,
-                nodes=1,
-                primary_network=network,
-            )
             .finalize()
         )
         self.conf = conf
