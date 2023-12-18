82c384f0494372b2ac24d1b1be69b281b9f57e97
Khaled Arsalane <contact@khaled.ovh>
2023-12-18 18:36:41 +0100

    Updating fish autocomplete

--- dockerfile/fish/shub.fish
+++ dockerfile/fish/shub.fish
@@ -1,11 +1,24 @@
 # shub.fish
 
+# Define default scalehub paths
+# SHUB_PATH: The root directory of the scalehub application
+set -gx SHUB_PATH /app
+# SHUB_CONF_PATH: The directory where the configuration files are stored
+set -gx SHUB_CONF_PATH $SHUB_PATH/conf
+# SHUB_PLAYBOOKS_PATH: The directory where the playbook files are stored
+set -gx SHUB_PLAYBOOKS_PATH $SHUB_PATH/playbooks/project
+# SHUB_EXPERIMENTS_DATA_PATH: The directory where the experiment data is stored
+set -gx SHUB_EXPERIMENTS_DATA_PATH $SHUB_PATH/experiments-data
+
+# Define the list of commands that can be used with the 'shub' script
 set -l shub_commands provision destroy deploy delete run export plot
+# Enable file completion for the 'shub' command
 complete -c shub -f
 
+# Enable autocompletion for the 'shub' command with the defined list of commands
 complete -c shub -n "not __fish_seen_subcommand_from $shub_commands" -a "provision destroy deploy delete reload run export plot"
 
-# Autocompletion for the 'shub' script
+# Define the descriptions for each command
 complete -c shub -n "__fish_use_subcommand" -a "provision" -d "Provision the platform specified in conf/scalehub.conf"
 complete -c shub -n "__fish_use_subcommand" -a "destroy" -d "Destroy the platform specified in conf/scalehub.conf"
 complete -c shub -n "__fish_use_subcommand" -a "deploy" -d "Execute deploy tasks of the provided playbook"
@@ -16,24 +29,45 @@ complete -c shub -n "__fish_use_subcommand" -a "export" -d "Export data"
 complete -c shub -n "__fish_use_subcommand" -a "plot" -d "Starts interactive plotter"
 
 # Autocompletion for 'deploy' and 'delete' commands
+# This function returns the names of the playbook files
 function __fish_shub_deploy_delete_complete
-    set playbook_files /app/playbooks/*.yaml
+    set playbook_files $SHUB_PLAYBOOKS_PATH/*.yaml
     for file in $playbook_files
-        complete --no-files --arguments=(basename $file .yaml) --condition="test (count (commandline)) = 2"
+        set filename (basename $file .yaml)
+        # If the file name begins with 'cluster-setup', skip it
+        if string match -q "cluster-setup*" $filename
+            continue
+        end
+        echo $filename
     end
 end
 
-complete --command shub --arguments '(__fish_shub_deploy_delete_complete)'
-#
-# # Autocompletion for 'run' command
-# complete --command shub --arguments '(string)' --condition="test (count (commandline)) = 2"
-#
-# # Autocompletion for 'export' command
-# function __fish_shub_export_complete
-#     set folder_names /app/experiments-data/*
-#     for folder in $folder_names
-#         complete --no-files --arguments=(basename $folder) --condition="test (count (commandline)) = 2"
-#     end
-# end
-#
-# complete --command shub --arguments '(__fish_shub_export_complete)'
+# Enable autocompletion for the 'deploy' and 'delete' commands with the playbook file names
+complete -c shub -n "__fish_seen_subcommand_from deploy delete reload" -a '(__fish_shub_deploy_delete_complete)'
+
+# Autocompletion for 'export' command
+# This function returns the names of the directories in SHUB_EXPERIMENTS_DATA_PATH
+function __fish_shub_export_complete
+    for dir in $SHUB_EXPERIMENTS_DATA_PATH/*
+        if test -d $dir
+            set dirname (basename $dir)
+            # If the directory name matches the 'DD-MM-YYYY' format, echo it
+            if string match -q -r '^[0-9]{2}-[0-9]{2}-[0-9]{4}$' $dirname
+                echo $dirname
+                # Iterate over the subdirectories of the current directory
+                for subdir in $dir/*
+                    if test -d $subdir
+                        set subdirname (basename $subdir)
+                        # If the subdirectory name is a number, echo it with the parent directory name
+                        if string match -q -r '^[0-9]+$' $subdirname
+                            echo $dirname/$subdirname
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+# Enable autocompletion for the 'export' command with the directory names
+complete -c shub -n "__fish_seen_subcommand_from export" -a '(__fish_shub_export_complete)'
