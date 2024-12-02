b48dc285802fb83c5878c55682ac809302aa3725
Khaled Arsalane <contact@khaled.ovh>
2024-12-02 20:51:45 +0100

    Fixing logic for job id and plan retrieval

--- scripts/src/resources/FlinkManager.py
+++ scripts/src/resources/FlinkManager.py
@@ -14,12 +14,19 @@ class FlinkManager:
 
     def get_job_id(self):
         try:
-            job_id = self.k.pod_manager.execute_command_on_pod(
-                deployment_name="flink-jobmanager",
-                command="flink list -r 2>/dev/null | grep RUNNING | awk '{print $4}'",
-            ).strip()
-            self.__log.info(f"[FLK_MGR] Job id: {job_id}")
-            return job_id
+            retry = 3
+            while retry > 0:
+                job_id = self.k.pod_manager.execute_command_on_pod(
+                    deployment_name="flink-jobmanager",
+                    command="flink list -r 2>/dev/null | grep RUNNING | awk '{print $4}'",
+                ).strip()
+                if job_id:
+                    self.__log.info(f"[FLK_MGR] Job id: {job_id}")
+                    return job_id
+
+                retry -= 1
+                sleep(5)
+            return None
         except Exception as e:
             self.__log.error(f"[FLK_MGR] Error while getting job id: {e}")
             return None
@@ -28,11 +35,17 @@ class FlinkManager:
         try:
             import requests
 
-            r = requests.get(
-                f"http://flink-jobmanager.flink.svc.cluster.local:8081/jobs/{job_id}/plan"
-            )
-            self.__log.info(f"[FLK_MGR] Job plan response: {r.text}")
-            return r.json()
+            retry = 3
+            while retry > 0:
+                r = requests.get(
+                    f"http://flink-jobmanager.flink.svc.cluster.local:8081/jobs/{job_id}/plan"
+                )
+                if r.status_code == 200:
+                    self.__log.info(f"[FLK_MGR] Job plan response: {r.text}")
+                    return r.json()
+                retry -= 1
+                sleep(5)
+            return None
         except Exception as e:
             self.__log.error(f"[FLK_MGR] Error while getting job plan: {e}")
             return None
@@ -76,28 +89,35 @@ class FlinkManager:
             job_id = self.get_job_id()
             job_plan = self.get_job_plan(job_id)
             operator_names = self.get_operator_names(job_plan)
-            retries = 10
-            savepoint_path = None
-            sleep_time = 3
-            while retries > 0 and savepoint_path is None:
-                resp = self.k.pod_manager.execute_command_on_pod(
-                    deployment_name="flink-jobmanager",
-                    command=f"flink stop -p -d {job_id}",
-                )
-                for line in resp.split("\n"):
-                    if "Savepoint completed." in line:
-                        savepoint_path = line.split("Path:")[1].strip()
-                        self.__log.info(f"[FLK_MGR] Savepoint path: {savepoint_path}")
-                        break
-                retries -= 1
-                # At each iteration increase sleep time
-                sleep_time += 1
-                sleep(sleep_time)
-            if savepoint_path is None:
-                self.__log.error("[FLK_MGR] Savepoint failed.")
+
+            if job_id is None or operator_names is None:
+                self.__log.error("[FLK_MGR] Job id or operator names not found.")
                 return None, None, None
-            sleep(5)
-            return job_id, operator_names, savepoint_path
+            else:
+                retries = 10
+                savepoint_path = None
+                sleep_time = 3
+                while retries > 0 and savepoint_path is None:
+                    resp = self.k.pod_manager.execute_command_on_pod(
+                        deployment_name="flink-jobmanager",
+                        command=f"flink stop -p -d {job_id}",
+                    )
+                    for line in resp.split("\n"):
+                        if "Savepoint completed." in line:
+                            savepoint_path = line.split("Path:")[1].strip()
+                            self.__log.info(
+                                f"[FLK_MGR] Savepoint path: {savepoint_path}"
+                            )
+                            break
+                    retries -= 1
+                    # At each iteration increase sleep time
+                    sleep_time += 1
+                    sleep(sleep_time)
+                if savepoint_path is None:
+                    self.__log.error("[FLK_MGR] Savepoint failed.")
+                    return None, None, None
+                sleep(5)
+                return job_id, operator_names, savepoint_path
         except Exception as e:
             self.__log.error(f"[FLK_MGR] Error while stopping job: {e}")
             return None, None, None
