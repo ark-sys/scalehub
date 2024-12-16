77efa1742ecb2b6895aaf7ec0fbff8a5ed16a244
Khaled Arsalane <contact@khaled.ovh>
2024-12-16 15:51:50 +0100

    - Refactoring FlinkManager - Adding multi slot scaling strategy

--- scripts/monitor/experiments/Scaling.py
+++ scripts/monitor/experiments/Scaling.py
@@ -26,9 +26,9 @@ class Scaling:
     def set_stopped_callback(self, stopped):
         self.__stopped = stopped
 
-    def __wait_interval(self, extra_time=0):
-        wait_time = self.interval_scaling_s + extra_time
-        self.__log.info(f"[SCALING] Waiting for {wait_time} seconds")
+    def __wait_interval(self):
+        wait_time = self.interval_scaling_s
+        self.__log.info(f"[SCALING] Monitoring interval: for {wait_time} seconds")
         # sleep unless stopped
         for i in range(wait_time):
             if self.__stopped is not None and self.__stopped():
@@ -37,42 +37,22 @@ class Scaling:
             sleep(1)
         return 0
 
-    def __scale_and_wait(self, tm_name, replicas):
-        self.__log.info(
-            f"[SCALING] ************ Scaling up {tm_name} to {replicas} ************"
-        )
-        self.k.statefulset_manager.scale_statefulset(
-            statefulset_name=tm_name, replicas=replicas, namespace="flink"
-        )
-        ret = self.f.rescale_job(replicas)
+    def __scale_and_wait(self, replicas):
+        self.__log.info(f"[SCALING] ************ Scaling up to {replicas} ************")
+        ret = self.f.run_job(replicas)
         if ret == 1:
             return 1
         else:
-            if tm_name == "flink-taskmanager-s":
-                # Small may take some time to fully startup, especially for Flink
-                return self.__wait_interval(extra_time=30)
-            else:
-                return self.__wait_interval()
-
-    # Add replicas linearly
-    def __scale_operator_linear(self, number, tm_type):
-        # Get the name of the stateful set to scale
-        tm_name = f"flink-taskmanager-{tm_type}"
-
-        # Get current number of taskmanagers
-        taskmanagers_count_dict = self.k.statefulset_manager.get_count_of_taskmanagers()
-        self.__log.info(
-            f"[SCALING] Current number of taskmanagers: {taskmanagers_count_dict}"
-        )
-        for i in range(number):
-            # Scale up stateful set
-            taskmanagers_count_dict[tm_type] += 1
-            ret = self.__scale_and_wait(tm_name, taskmanagers_count_dict[tm_type])
+            # Rescale successful, populate job info
+            self.f.get_job_info()
+            self.f.check_nominal_job_run()
+            ret = self.f.wait_for_job_running()
             if ret == 1:
                 return 1
+            return self.__wait_interval()
 
-    # Add replicas exponentially
-    def __scale_operator_exponential(self, number, tm_type):
+    def __scale_w_tm(self, replicas, tm_type):
+        self.__log.info(f"[SCALING] ************ Scaling up to {replicas} ************")
         # Get the name of the stateful set to scale
         tm_name = f"flink-taskmanager-{tm_type}"
 
@@ -81,7 +61,50 @@ class Scaling:
         self.__log.info(
             f"[SCALING] Current number of taskmanagers: {taskmanagers_count_dict}"
         )
+        # Scale up stateful set
+        taskmanagers_count_dict[tm_type] += replicas
+        self.k.statefulset_manager.scale_statefulset(
+            statefulset_name=tm_name,
+            replicas=taskmanagers_count_dict[tm_type],
+            namespace="flink",
+        )
+        ret = self.__scale_and_wait(taskmanagers_count_dict[tm_type])
+        if ret == 1:
+            return 1
 
+    # Add replicas linearly
+    def __scale_linear(self, number, tm_type, scope):
+        match scope:
+            case "taskmanager":
+                for i in range(number):
+                    # Scale up stateful set
+                    ret = self.__scale_w_tm(1, tm_type)
+                    if ret == 1:
+                        return 1
+            case "slots":
+                # Get current parallelism of monitored task
+                current_parallelism = self.f.monitored_task_parallelism
+                self.__log.info(
+                    f"[SCALING] Current parallelism of monitored task: {current_parallelism}"
+                )
+                for i in range(number):
+                    # Scale up operator
+                    current_parallelism += 1
+                    ret = self.__scale_and_wait(current_parallelism)
+                    if ret == 1:
+                        return 1
+            case _:
+                self.__log.warning(
+                    f"[SCALING] Scope {scope} not supported. Defaulting to taskmanager."
+                )
+                for i in range(number):
+                    # Scale up stateful set
+                    ret = self.__scale_w_tm(1, tm_type)
+                    if ret == 1:
+                        return 1
+
+    # Add replicas exponentially
+    def __scale_exponential(self, number, tm_type, scope):
         def __get_scaling_sequence(seq_n):
             pascaline_sequence = [1]
             i = 1
@@ -93,47 +116,75 @@ class Scaling:
             return pascaline_sequence
 
         scaline_sequence = __get_scaling_sequence(number)
-        self.__log.info(f"[SCALING] Scaling sequence: {scaline_sequence}")
 
-        for i in scaline_sequence:
-            # Scale up stateful set
-            taskmanagers_count_dict[tm_type] += i
-            ret = self.__scale_and_wait(tm_name, taskmanagers_count_dict[tm_type])
-            if ret == 1:
-                return 1
+        match scope:
+            case "taskmanager":
+                for i in scaline_sequence:
+                    # Scale up stateful set
+                    ret = self.__scale_w_tm(i, tm_type)
+                    if ret == 1:
+                        return 1
+            case "slots":
+                # Get current parallelism of monitored task
+                current_parallelism = self.f.monitored_task_parallelism
+                self.__log.info(
+                    f"[SCALING] Current parallelism of monitored task: {current_parallelism}"
+                )
+                for i in scaline_sequence:
+                    # Scale up operator
+                    current_parallelism += i
+                    ret = self.__scale_and_wait(current_parallelism)
+                    if ret == 1:
+                        return 1
+            case _:
+                self.__log.warning(
+                    f"[SCALING] Scope {scope} not supported. Defaulting to taskmanager."
+                )
+                for i in scaline_sequence:
+                    # Scale up stateful set
+                    ret = self.__scale_w_tm(i, tm_type)
+                    if ret == 1:
+                        return 1
 
     # Add replicas at once
-    def __scale_operator_block(self, number, tm_type):
-        # Get the name of the stateful set to scale
-        tm_name = f"flink-taskmanager-{tm_type}"
-        # Get current number of taskmanagers
-        taskmanagers_count_dict = self.k.statefulset_manager.get_count_of_taskmanagers()
-        self.__log.info(
-            f"[SCALING] Current number of taskmanagers: {taskmanagers_count_dict}"
-        )
-        # Scale up stateful set
-        taskmanagers_count_dict[tm_type] += number
-        ret = self.__scale_and_wait(tm_name, taskmanagers_count_dict[tm_type])
-        if ret == 1:
-            return 1
+    def __scale_block(self, number, tm_type, scope):
+        match scope:
+            case "taskmanager":
+                # Get current number of taskmanagers
+                return self.__scale_w_tm(number, tm_type)
+            case "slots":
+                # Get current parallelism of monitored task
+                current_parallelism = self.f.monitored_task_parallelism
+                self.__log.info(
+                    f"[SCALING] Current parallelism of monitored task: {current_parallelism}"
+                )
+                # Scale up operator
+                new_parallelism = current_parallelism + number
+                return self.__scale_and_wait(new_parallelism)
+            case _:
+                self.__log.warning(
+                    f"[SCALING] Scope {scope} not supported. Defaulting to taskmanager."
+                )
+                return self.__scale_w_tm(number, tm_type)
 
-    def __scale_op(self, taskmanager):
+    def __scale(self, taskmanager):
         number = taskmanager["number"]
         tm_type = taskmanager["type"]
         scaling_method = taskmanager["method"]
+        scope = taskmanager["scope"]
 
         match scaling_method:
             case "linear":
-                ret = self.__scale_operator_linear(number, tm_type)
+                ret = self.__scale_linear(number, tm_type)
             case "exponential":
-                ret = self.__scale_operator_exponential(number, tm_type)
+                ret = self.__scale_exponential(number, tm_type)
             case "block":
-                ret = self.__scale_operator_block(number, tm_type)
+                ret = self.__scale_block(number, tm_type, scope)
             case _:
                 self.__log.warning(
                     f"[SCALING] Scaling method {scaling_method} not supported. Defaulting to linear."
                 )
-                ret = self.__scale_operator_linear(number, tm_type)
+                ret = self.__scale_linear(number, tm_type)
         if ret == 1:
             return 1
 
@@ -146,7 +197,7 @@ class Scaling:
 
         taskmanagers = self.steps[step]["taskmanager"]
         for taskmanager in taskmanagers:
-            ret = self.__scale_op(taskmanager)
+            ret = self.__scale(taskmanager)
             if ret == 1:
                 return 1
 
@@ -226,6 +277,9 @@ class Scaling:
         if ret == 1:
             return 1
 
+        # Populate job info
+        self.f.get_job_info()
+
         return first_node
 
     def run(self):
@@ -233,7 +287,7 @@ class Scaling:
         if node_name == 1:
             return 1
 
-        self.__log.info("[SCALING] First taskmanager, just waiting")
+        self.__log.info("[SCALING] First taskmanager, just waiting...")
         ret = self.__wait_interval()
         if ret == 1:
             return 1
--- scripts/src/resources/FlinkManager.py
+++ scripts/src/resources/FlinkManager.py
@@ -1,3 +1,4 @@
+import re
 from time import sleep
 
 from scripts.src.resources.KubernetesManager import KubernetesManager
@@ -14,6 +15,15 @@ class FlinkManager:
         self.flink_host = "flink-jobmanager.flink.svc.cluster.local"
         self.flink_port = 8081
 
+        # Store running job information
+        self.monitored_task = self.config.get_str(Key.Experiment.task_name)
+        self.job_file = self.config.get_str(Key.Experiment.job_file)
+        self.job_plan = None
+        self.job_id = None
+        self.operator_names = None
+        self.monitored_task_parallelism = None
+        self.savepoint_path = None
+
     def __get_overview(self):
         try:
             import requests
@@ -26,38 +36,7 @@ class FlinkManager:
             self.__log.error(f"[FLK_MGR] Error while getting overview: {e}")
             return None
 
-    def get_total_slots(self):
-        overview = self.__get_overview()
-        if overview is not None:
-            return overview["slots-total"]
-        return None
-
-    def get_total_taskmanagers(self):
-        overview = self.__get_overview()
-        if overview is not None:
-            return overview["taskmanagers"]
-        return None
-
-    def get_job_id(self):
-        try:
-            retry = 3
-            while retry > 0:
-                job_id = self.k.pod_manager.execute_command_on_pod(
-                    deployment_name="flink-jobmanager",
-                    command="flink list -r 2>/dev/null | grep RUNNING | awk '{print $4}'",
-                ).strip()
-                if job_id:
-                    self.__log.info(f"[FLK_MGR] Job id: {job_id}")
-                    return job_id
-
-                retry -= 1
-                sleep(5)
-            return None
-        except Exception as e:
-            self.__log.error(f"[FLK_MGR] Error while getting job id: {e}")
-            return None
-
-    def get_job_plan(self, job_id):
+    def __get_job_plan(self, job_id):
         try:
             import requests
 
@@ -76,10 +55,26 @@ class FlinkManager:
             self.__log.error(f"[FLK_MGR] Error while getting job plan: {e}")
             return None
 
-    def get_operator_names(self, job_plan):
+    def __get_job_state(self):
+        # retrieve status of the job
+        try:
+            import requests
+
+            r = requests.get(
+                f"http://flink-jobmanager.flink.svc.cluster.local:8081/jobs/{self.job_id}/status"
+            )
+            if r.status_code == 200:
+                return r.json()["properties"]["status"]
+            else:
+                return None
+        except Exception as e:
+            self.__log.error(f"[FLK_MGR] Error while getting job state: {e}")
+            return None
+
+    def __get_operator_names(self):
         try:
             operator_names = []
-            for node in job_plan["plan"]["nodes"]:
+            for node in self.job_plan["plan"]["nodes"]:
                 operator_name = (
                     node["description"]
                     .replace("</br>", "")
@@ -93,32 +88,17 @@ class FlinkManager:
             self.__log.error(f"[FLK_MGR] Error while getting operator names: {e}")
             return None
 
-    def get_operator_parallelism(self, job_plan):
-        try:
-            operator_parallelism = {}
-            for node in job_plan["plan"]["nodes"]:
-                operator_name = (
-                    node["description"]
-                    .replace("</br>", "")
-                    .replace("<br/>", "")
-                    .replace(":", "_")
-                    .replace(" ", "_")
-                )
-                operator_parallelism[operator_name] = node["parallelism"]
-            return operator_parallelism
-        except Exception as e:
-            self.__log.error(f"[FLK_MGR] Error while getting operator parallelism: {e}")
-            return None
+    def __get_monitored_task_parallelism(self):
+        for operator in self.operator_names:
+            if self.monitored_task in operator:
+                return int(operator.split(":")[1])
 
-    def stop_job(self):
+    def __stop_job(self):
         try:
-            job_id = self.get_job_id()
-            job_plan = self.get_job_plan(job_id)
-            operator_names = self.get_operator_names(job_plan)
 
-            if job_id is None or operator_names is None:
-                self.__log.error("[FLK_MGR] Job id or operator names not found.")
-                return None, None, None
+            if self.job_id is None:
+                self.__log.error("[FLK_MGR] Job id not found.")
+                return None
             else:
                 retries = 10
                 savepoint_path = None
@@ -126,7 +106,7 @@ class FlinkManager:
                 while retries > 0 and savepoint_path is None:
                     resp = self.k.pod_manager.execute_command_on_pod(
                         deployment_name="flink-jobmanager",
-                        command=f"flink stop -p -d {job_id}",
+                        command=f"flink stop -p -d {self.job_id}",
                     )
                     for line in resp.split("\n"):
                         if "Savepoint completed." in line:
@@ -141,59 +121,116 @@ class FlinkManager:
                     sleep(sleep_time)
                 if savepoint_path is None:
                     self.__log.error("[FLK_MGR] Savepoint failed.")
-                    return None, None, None
+                    return None
                 sleep(5)
-                return job_id, operator_names, savepoint_path
+                return savepoint_path
         except Exception as e:
             self.__log.error(f"[FLK_MGR] Error while stopping job: {e}")
-            return None, None, None
+            return None
 
-    def run_job(self):
+    def __build_par_map(self, new_parallelism):
+        par_map = []
+        for operator_name in self.operator_names:
+            if self.monitored_task in operator_name:
+                par_map.append(f"{operator_name}:{new_parallelism}")
+            else:
+                par_map.append(f"{operator_name}:1")
+        return par_map
+
+    def run_job(self, new_parallelism=None):
         try:
-            # Get name of job file to run
-            job_file = self.config.get_str(Key.Experiment.job_file)
-            self.k.pod_manager.execute_command_on_pod(
-                deployment_name="flink-jobmanager",
-                command=f"flink run -d -j /tmp/jobs/{job_file}",
-            )
+            self.__log.info("[FLK_MGR] Running job.")
+            if new_parallelism is not None:
+                self.__log.info(f"[FLK_MGR] Rescaling job to {new_parallelism}.")
+                self.savepoint_path = self.__stop_job()
+                if self.savepoint_path is None:
+                    self.__log.error("[FLK_MGR] Savepoint failed.")
+                    return 1
+                sleep(10)
+                par_map = self.__build_par_map(new_parallelism)
+                res = self.k.pod_manager.execute_command_on_pod(
+                    deployment_name="flink-jobmanager",
+                    command=f"flink run -d -s {self.savepoint_path} -j /tmp/jobs/{self.job_file} --parmap '{';'.join(par_map)}'",
+                )
+                self.__log.info(
+                    f"[FLK_MGR] Operator {self.monitored_task} rescaled to {new_parallelism}."
+                )
+            else:
+                # Simply run the job
+                res = self.k.pod_manager.execute_command_on_pod(
+                    deployment_name="flink-jobmanager",
+                    command=f"flink run -d -j /tmp/jobs/{self.job_file}",
+                )
+            # Extract job id from response
+            self.job_id = re.search(r"JobID ([a-f0-9]+)", res).group(1)
+            if self.job_id:
+                self.__log.info(f"[FLK_MGR] Running job id: {self.job_id}")
+            else:  # Job id not found
+                self.__log.error("[FLK_MGR] Job id not found.")
+                return 1
         except Exception as e:
             self.__log.error(f"[FLK_MGR] Error while running job: {e}")
             return 1
+
+    def get_total_slots(self):
+        overview = self.__get_overview()
+        if overview is not None:
+            return overview["slots-total"]
+        return None
+
+    def get_total_taskmanagers(self):
+        overview = self.__get_overview()
+        if overview is not None:
+            return overview["taskmanagers"]
+        return None
+
+    def check_nominal_job_run(self):
+        # List current running jobs. If we multiple jobs running beside self.job_id. Cancel all of them
+        res = self.k.pod_manager.execute_command_on_pod(
+            deployment_name="flink-jobmanager",
+            command="flink list -r 2>/dev/null'",
+        ).strip()
+
+        # Extract job ids from response
+        job_ids = re.findall(r"\b\w{16,}\b", res)
+        for job_id in job_ids:
+            if job_id != self.job_id:
+                self.k.pod_manager.execute_command_on_pod(
+                    deployment_name="flink-jobmanager",
+                    command=f"flink cancel {job_id}",
+                )
+                self.__log.info(f"[FLK_MGR] Job {job_id} cancelled.")
+
         return 0
 
-    def scale_job_with_savepoint(self, operator_names, savepoint_path, new_parallelism):
+    def get_job_info(self):
         try:
-            # Get monitored task
-            monitored_task = self.config.get_str(Key.Experiment.task_name)
-
-            # Get name of job file to run
-            job_file = self.config.get_str(Key.Experiment.job_file)
-
-            # Build new par_map
-            par_map = []
-            for operator_name in operator_names:
-                if monitored_task in operator_name:
-                    par_map.append(f"{operator_name}:{new_parallelism}")
-                else:
-                    par_map.append(f"{operator_name}:1")
-
-            # Rescale job from savepoint
-            return self.k.pod_manager.execute_command_on_pod(
-                deployment_name="flink-jobmanager",
-                command=f"flink run -d -s {savepoint_path} -j /tmp/jobs/{job_file} --parmap '{';'.join(par_map)}'",
-            )
-        except Exception as e:
-            self.__log.error(f"[FLK_MGR] Error while rescaling job: {e}")
-
-    def rescale_job(self, new_parallelism):
-        try:
-            job_id, operator_names, savepoint_path = self.stop_job()
-            if savepoint_path is None:
-                self.__log.error("[FLK_MGR] Savepoint failed.")
+            self.__log.info("[FLK_MGR] Getting job info.")
+            if self.job_id is None:
+                self.__log.error("[FLK_MGR] Job id not found.")
                 return None
-            sleep(10)
-            return self.scale_job_with_savepoint(
-                operator_names, savepoint_path, new_parallelism
-            )
+            self.job_plan = self.__get_job_plan(self.job_id)
+            self.operator_names = self.__get_operator_names()
+            self.monitored_task_parallelism = self.__get_monitored_task_parallelism()
+
+            if self.operator_names is None:
+                self.__log.error("[FLK_MGR] Operator names not found.")
+                return None
         except Exception as e:
-            self.__log.error(f"[FLK_MGR] Error while rescaling job: {e}")
+            self.__log.error(f"[FLK_MGR] Error while getting job info: {e}")
+            return None
+
+    def wait_for_job_running(self):
+        job_state = None
+        try:
+            retries = 10
+            while retries > 0 and job_state != "RUNNING":
+                job_state = self.__get_job_state()
+                if job_state == "RUNNING":
+                    return 0
+                retries -= 1
+                sleep(5)
+            return 1
+        except Exception as e:
+            self.__log.error(f"[FLK_MGR] Error while waiting for job to run: {e}")
+            return 1
