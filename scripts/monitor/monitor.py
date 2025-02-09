1e039576168a616cf4a49dd792e62553eb4d8e64
Khaled Arsalane <contact@khaled.ovh>
2025-02-09 22:18:10 +0100

    - Refactor code - Updated version for data management - Adding gitlab agent connection

--- scripts/monitor/monitor.py
+++ scripts/monitor/monitor.py
@@ -3,7 +3,6 @@ import os
 import threading
 
 import paho.mqtt.client as mqtt
-
 # noinspection PyUnresolvedReferences
 from paho.mqtt.enums import CallbackAPIVersion
 
@@ -33,7 +32,7 @@ class MQTTClient(threading.Thread):
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
 
-    def on_connect(self, connect_flags):
+    def on_connect(self, client, userdata, connect_flags, reason_code, properties):
         self.__log.info(f"[CLIENT] Connected with result code {connect_flags}")
 
         # Subscribe to experiment command topic
@@ -50,7 +49,7 @@ class MQTTClient(threading.Thread):
             return False
         return True
 
-    def on_message(self, msg):
+    def on_message(self, client, userdata, msg):
         if msg.topic == "experiment/command":
             # Check if payload is in json format
             self.__log.info(
